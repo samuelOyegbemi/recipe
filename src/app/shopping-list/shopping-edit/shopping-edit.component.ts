9d9957aceb16a27020995ef47ae7229fe14e70f1
Samuel Oyegbemi <samueloyegbemi@gmail.com>
2018-08-10 14:07:52 +0100

    this is a new repository

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../general/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  
+  ingredientName: string = "";
+  ingredientAmount: number = 0;
+  ingredient: Ingredient = new Ingredient("", 0);
   constructor() { }
 
   ngOnInit() {
   }
-
 }
